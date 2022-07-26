5a762690b8384a5fddf8cb5c5b180050ffdff9ec
solmazHmercimek <109738354+solmazHmercimek@users.noreply.github.com>
2022-07-26 13:48:42 +0300

    FirstStep

---
+++ src/main/java/com/javaCourse/project/hibernateAndJpa/DataAccess/HibernateCityDal.java
@@ -0,0 +1,48 @@
+package com.javaCourse.project.hibernateAndJpa.DataAccess;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.javaCourse.project.hibernateAndJpa.Entities.City;
+//Jpa eriişim tekniği olarak çıkıp.Java standardı haline geldi. 
+public class HibernateCityDal implements ICityDal{
+	
+	public EntityManager entityManager;
+	//OtOmatij olarak enttity manager paketlerinin ne olduğuna bakar ve geri dönüş sağlar
+	@Autowired
+	public HibernateCityDal(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
+
+	@Override
+	@Transactional
+	public List<City> getAll() {
+		Session session = entityManager.unwrap(Session.class);
+		List<City> cities = session.createQuery("from City",City.class).getResultList();
+		return cities;
+	}
+
+	@Override
+	public void add(City city) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(City city) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(City city) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
